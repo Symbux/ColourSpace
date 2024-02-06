842e4671efcfbed0ee8acd279d5c346b15dfa649
dannysmc95 <danny.smc95@gmail.com>
2024-02-06 15:58:45 +0000

    Added extra tsdoc/jsdoc, commented the helper methods, and switched the fromX methods to private, as they can't be used without context of the class anyway.

--- src/colourspace.ts
+++ src/colourspace.ts
@@ -1,5 +1,17 @@
 import type { IColourSpaceType, IColourSpaceMap, IColourSpaceConverterMap } from './types';
 
+/**
+ * The ColourSpace class will allow you to convert between
+ * HEX, RGB and HSL colour formats.
+ * @class
+ * @public
+ * @example
+ * ```typescript
+ * const instance = new ColourSpace('#4F933E');
+ * console.log(instance.toRgb()); // rgb(79, 147, 62)
+ * console.log(instance.toHsl()); // hsl(108, 41%, 41%)
+ * ```
+ */
 export class ColourSpace {
 	private colour: IColourSpaceMap;
 	private mapping: IColourSpaceConverterMap = {
@@ -12,75 +24,17 @@ export class ColourSpace {
 	 * Create an instance of the colour space class.
 	 * @param colour The colour to convert.
 	 * @param type [Optional] The format of the input colour, will be guessed if not provided.
+	 * @public
 	 */
 	public constructor(colour: string, type?: IColourSpaceType) {
 		this.colour = this.processColour(colour, type);
 	}
 
 	/**
-	 * Will convert a HEX / HEXA string to the internal
-	 * object structure.
-	 * @param hex The HEX/HEXA string to convert.
-	 * @returns IColourSpaceMap
-	 */
-	public fromHex(hex: string): IColourSpaceMap {
-		hex = hex.startsWith('#') ? hex : `#${hex}`;
-		const hexa = hex.length === 9;
-		const alpha = hexa ? parseFloat((parseInt(hex.slice(7, 9), 16) / 255).toPrecision(2)) : 1;
-		const red = parseInt(hex.slice(1, 3), 16);
-		const green = parseInt(hex.slice(3, 5), 16);
-		const blue = parseInt(hex.slice(5, 7), 16);
-		return { red, green, blue, alpha };
-	}
-
-	/**
-	 * Will convert an RGB / RGBA string to the internal
-	 * object structure.
-	 * @param rgb The RGB/RGBA string to convert.
-	 * @returns IColourSpaceMap
-	 */
-	public fromRgb(rgb: string) {
-		let [red, green, blue, alpha] = rgb.split(rgb.includes(',') ? ',' : ' ')
-			.filter(v => v !== '/')
-			.map(v => v.replace(/[^0-9.]/g, ''))
-			.map(v => v.includes('.') ? parseFloat(v) : parseInt(v));
-
-		if (rgb.includes('/')) alpha = alpha / 100;
-		return { red, green, blue, alpha: alpha ?? 1 };
-	}
-
-	/**
-	 * Will convert an HSL / HSLA string to the internal
-	 * object structure.
-	 * @param hsl The HSL/HSLA string to convert.
-	 * @returns ICoulourSpaceMap
-	 */
-	public fromHsl(hsl: string) {
-		let [hue, saturation, lightness, alpha] = hsl.split(hsl.includes(',') ? ',' : ' ')
-			.filter(v => v !== '/')
-			.map(v => v.replace(/[^0-9.]/g, ''))
-			.map(v => v.includes('.') ? parseFloat(v) : parseInt(v));
-
-		if (hsl.includes('/')) alpha = alpha / 100;
-		saturation /= 100;
-		lightness /= 100;
-
-		const k = (n: number) => (n + hue / 30) % 12;
-		const a = saturation * Math.min(lightness, 1 - lightness);
-		const f = (n: number) => lightness - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-
-		return {
-			red: Math.round(255 * f(0)),
-			green: Math.round(255 * f(8)),
-			blue: Math.round(255 * f(4)),
-			alpha: alpha ?? 1,
-		};
-	}
-
-	/**
 	 * Will convert the internal object structure to an HEX string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toHex(unwrap = false) {
 		const { red, green, blue } = this.colour;
@@ -92,6 +46,7 @@ export class ColourSpace {
 	 * Will convert the internal object structure to an HEXA string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toHexa(unwrap = false) {
 		const { red, green, blue, alpha } = this.colour;
@@ -103,6 +58,7 @@ export class ColourSpace {
 	 * Will convert the internal object structure to an RGB string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toRgb(unwrap = false) {
 		const { red, green, blue } = this.colour;
@@ -114,6 +70,7 @@ export class ColourSpace {
 	 * Will convert the internal object structure to an RGBA string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toRgba(unwrap = false) {
 		const { red, green, blue, alpha } = this.colour;
@@ -125,6 +82,7 @@ export class ColourSpace {
 	 * Will convert the internal object structure to an HSL string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toHsl(unwrap = false) {
 		let { red, green, blue } = this.colour;
@@ -159,6 +117,7 @@ export class ColourSpace {
 	 * Will convert the internal object structure to an HSLA string.
 	 * @param unwrap Whether to unwrap the value.
 	 * @returns string
+	 * @public
 	 */
 	public toHsla(unwrap = false) {
 		const { alpha } = this.colour;
@@ -173,6 +132,7 @@ export class ColourSpace {
 	 * convert to the desired format.
 	 * @param override [Optional] The colours to use for the contrast.
 	 * @returns ColourSpace
+	 * @public
 	 */
 	public toContrast(override?: { dark?: string; light?: string }) {
 		const { red, green, blue } = this.colour;
@@ -183,11 +143,75 @@ export class ColourSpace {
 	}
 
 	/**
-	 * Simply returns the internal object structure.
+	 * Returns the internal object structure.
 	 * @returns IColourSpaceMap
+	 * @public
 	 */
 	public getRaw() {
 		return this.colour;
+	}
+
+	/**
+	 * Will convert a HEX / HEXA string to the internal
+	 * object structure.
+	 * @param hex The HEX/HEXA string to convert.
+	 * @returns IColourSpaceMap
+	 * @private
+	 */
+	private fromHex(hex: string): IColourSpaceMap {
+		hex = hex.startsWith('#') ? hex : `#${hex}`;
+		const hexa = hex.length === 9;
+		const alpha = hexa ? parseFloat((parseInt(hex.slice(7, 9), 16) / 255).toPrecision(2)) : 1;
+		const red = parseInt(hex.slice(1, 3), 16);
+		const green = parseInt(hex.slice(3, 5), 16);
+		const blue = parseInt(hex.slice(5, 7), 16);
+		return { red, green, blue, alpha };
+	}
+
+	/**
+	 * Will convert an RGB / RGBA string to the internal
+	 * object structure.
+	 * @param rgb The RGB/RGBA string to convert.
+	 * @returns IColourSpaceMap
+	 * @private
+	 */
+	private fromRgb(rgb: string) {
+		let [red, green, blue, alpha] = rgb.split(rgb.includes(',') ? ',' : ' ')
+			.filter(v => v !== '/')
+			.map(v => v.replace(/[^0-9.]/g, ''))
+			.map(v => v.includes('.') ? parseFloat(v) : parseInt(v));
+
+		if (rgb.includes('/')) alpha = alpha / 100;
+		return { red, green, blue, alpha: alpha ?? 1 };
+	}
+
+	/**
+	 * Will convert an HSL / HSLA string to the internal
+	 * object structure.
+	 * @param hsl The HSL/HSLA string to convert.
+	 * @returns ICoulourSpaceMap
+	 * @private
+	 */
+	private fromHsl(hsl: string) {
+		let [hue, saturation, lightness, alpha] = hsl.split(hsl.includes(',') ? ',' : ' ')
+			.filter(v => v !== '/')
+			.map(v => v.replace(/[^0-9.]/g, ''))
+			.map(v => v.includes('.') ? parseFloat(v) : parseInt(v));
+
+		if (hsl.includes('/')) alpha = alpha / 100;
+		saturation /= 100;
+		lightness /= 100;
+
+		const k = (n: number) => (n + hue / 30) % 12;
+		const a = saturation * Math.min(lightness, 1 - lightness);
+		const f = (n: number) => lightness - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+
+		return {
+			red: Math.round(255 * f(0)),
+			green: Math.round(255 * f(8)),
+			blue: Math.round(255 * f(4)),
+			alpha: alpha ?? 1,
+		};
 	}
 
 	/**
@@ -195,6 +219,7 @@ export class ColourSpace {
 	 * internal object structure.
 	 * @param colour The colour string.
 	 * @param type [Optional] The format of the input colour, will be guessed if not provided.
+	 * @private
 	 */
 	private processColour(colour: string, type?: IColourSpaceType) {
 		const vtype = type ?? this.guessFormat(colour);
@@ -208,6 +233,7 @@ export class ColourSpace {
 	 * leading definition.
 	 * @param value The value to guess the format of.
 	 * @returns ICoulourSpaceType
+	 * @private
 	 */
 	private guessFormat(value: string) {
 		if (value.startsWith('#')) return 'hex';
@@ -221,6 +247,7 @@ export class ColourSpace {
 	 * in length.
 	 * @param value The value to pad.
 	 * @returns string
+	 * @private
 	 */
 	private padLeft(value: string) {
 		return value.length === 1 ? `0${value}` : value;
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,97 @@
 import { ColourSpace } from './colourspace';
 
-// Hex To X
-export const hexToRgb = (hex: string): string => new ColourSpace(hex).toRgb();
-export const hexToRgba = (hex: string): string => new ColourSpace(hex).toRgba();
-export const hexToHsl = (hex: string): string => new ColourSpace(hex).toHsl();
-export const hexToHsla = (hex: string): string => new ColourSpace(hex).toHsla();
+/**
+ * Converts a HEX/HEXA string to an RGB string.
+ * @param hex The HEX/HEXA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hexToRgb = (hex: string, unwrap = false): string => new ColourSpace(hex).toRgb(unwrap);
 
-// RGB To X
-export const rgbToHex = (rgb: string): string => new ColourSpace(rgb, 'rgb').toHex();
-export const rgbToHexa = (rgb: string): string => new ColourSpace(rgb, 'rgb').toHexa();
-export const rgbToHsl = (rgb: string): string => new ColourSpace(rgb, 'rgb').toHsl();
-export const rgbToHsla = (rgb: string): string => new ColourSpace(rgb, 'rgb').toHsla();
+/**
+ * Converts a HEX/HEXA string to an RGBA string.
+ * @param hex The HEX/HEXA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hexToRgba = (hex: string, unwrap = false): string => new ColourSpace(hex).toRgba(unwrap);
 
-// HSL To X
-export const hslToHex = (hsl: string): string => new ColourSpace(hsl, 'hsl').toHex();
-export const hslToHexa = (hsl: string): string => new ColourSpace(hsl, 'hsl').toHexa();
-export const hslToRgb = (hsl: string): string => new ColourSpace(hsl, 'hsl').toRgb();
-export const hslToRgba = (hsl: string): string => new ColourSpace(hsl, 'hsl').toRgba();
+/**
+ * Converts a HEX/HEXA string to an HSL string.
+ * @param hex The HEX/HEXA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hexToHsl = (hex: string, unwrap = false): string => new ColourSpace(hex).toHsl(unwrap);
+
+/**
+ * Converts a HEX/HEXA string to an HSLA string.
+ * @param hex The HEX/HEXA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hexToHsla = (hex: string, unwrap = false): string => new ColourSpace(hex).toHsla(unwrap);
+
+/**
+ * Converts a RGB/RGBA string to an HEX string.
+ * @param hex The RGB/RGBA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const rgbToHex = (rgb: string, unwrap = false): string => new ColourSpace(rgb, 'rgb').toHex(unwrap);
+
+/**
+ * Converts a RGB/RGBA string to an HEXA string.
+ * @param hex The RGB/RGBA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const rgbToHexa = (rgb: string, unwrap = false): string => new ColourSpace(rgb, 'rgb').toHexa(unwrap);
+
+/**
+ * Converts a RGB/RGBA string to an HSL string.
+ * @param hex The RGB/RGBA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const rgbToHsl = (rgb: string, unwrap = false): string => new ColourSpace(rgb, 'rgb').toHsl(unwrap);
+
+/**
+ * Converts a RGB/RGBA string to an HSLA string.
+ * @param hex The RGB/RGBA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const rgbToHsla = (rgb: string, unwrap = false): string => new ColourSpace(rgb, 'rgb').toHsla(unwrap);
+
+/**
+ * Converts a HSL/HSLA string to an HEX string.
+ * @param hex The HSL/HSLA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hslToHex = (hsl: string, unwrap = false): string => new ColourSpace(hsl, 'hsl').toHex(unwrap);
+
+/**
+ * Converts a HSL/HSLA string to an HEXA string.
+ * @param hex The HSL/HSLA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hslToHexa = (hsl: string, unwrap = false): string => new ColourSpace(hsl, 'hsl').toHexa(unwrap);
+
+/**
+ * Converts a HSL/HSLA string to an RGB string.
+ * @param hex The HSL/HSLA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hslToRgb = (hsl: string, unwrap = false): string => new ColourSpace(hsl, 'hsl').toRgb(unwrap);
+
+/**
+ * Converts a HSL/HSLA string to an RGBA string.
+ * @param hex The HSL/HSLA string.
+ * @param unwrap Whether to unwrap the value.
+ * @returns string
+ */
+export const hslToRgba = (hsl: string, unwrap = false): string => new ColourSpace(hsl, 'hsl').toRgba(unwrap);
